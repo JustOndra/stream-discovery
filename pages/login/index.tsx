043445e4433110a5ae5c235b7f64c506ca58c1ff
Ondřej Šimanovský <simanovskyondrej@gmail.com>
2023-06-18 20:54:33 +0200

    Initial commit from create-refine-app

---
+++ pages/login/index.tsx
@@ -0,0 +1,39 @@
+import { AuthPage } from "@refinedev/mui";
+
+import { GetServerSideProps } from "next";
+
+import { authProvider } from "src/authProvider";
+
+export default function Login() {
+  return (
+    <AuthPage
+      type="login"
+      formProps={{
+        defaultValues: {
+          email: "info@refine.dev",
+          password: "refine-supabase",
+        },
+      }}
+    />
+  );
+}
+
+Login.noLayout = true;
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated } = await authProvider.check(context);
+
+  if (authenticated) {
+    return {
+      props: {},
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
